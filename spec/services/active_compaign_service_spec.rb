ae9caf917ce12b10a3a50752b19e7707f401b10e
hussainakram <hussain.akram128@gmail.com>
2021-06-09 02:04:20 +0500

    add vcr test for create_contact

--- spec/services/active_compaign_service_spec.rb
+++ spec/services/active_compaign_service_spec.rb
@@ -3,15 +3,26 @@ require "rails_helper"
 
 describe ActiveCompaignService do
   context "manage contacts" do
-    let(:user) { double(:user) }
-    let(:service_object) { instance_double(ActiveCompaignService) }
+    let!(:user) { double(:user) }
+    let!(:real_user) { create(:user, email: "test_2@test.com") }
+    let!(:service_object) { instance_double(ActiveCompaignService) }
 
     it "initializes class object with user" do
       allow(ActiveCompaignService).to(receive(:new).with(user))
     end
 
     it "#create_contact" do
-      allow(service_object).to(receive(:create_contact))
+      VCR.use_cassette("create_contact") do
+        service_object = described_class.new(real_user)
+
+        result = service_object.create_contact
+
+        allow(service_object).to(receive(:create_contact))
+        expect(result.is_a?(Hash)).to(be_truthy)
+        expect(result["email"]).to(eq(real_user.email))
+        expect(result["firstName"]).to(eq(real_user.first_name))
+        expect(result["lastName"]).to(eq(real_user.last_name))
+      end
     end
 
     it "#subscribe_contact_to_list" do
