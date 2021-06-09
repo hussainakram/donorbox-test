afdc3e830281ff051ce932dc72b733604d77a2ec
hussainakram <hussain.akram128@gmail.com>
2021-06-09 15:53:34 +0500

    use default mail sender for dev and test environment

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = ENV["SUPPORT_EMAIL"]
+  config.mailer_sender = Rails.env.production? ? ENV["SUPPORT_EMAIL"] : 'user@example.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
