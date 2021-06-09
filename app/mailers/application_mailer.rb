a52133f95a551550650b03e2c633a4c26528b955
Hussain Akram <hussain.akram128@gmail.com>
2021-06-09 15:58:02 +0500

    Add AWS SES for sending emails (#13)
    * Add AWS SES for sending emails

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 class ApplicationMailer < ActionMailer::Base
+  default "Message-ID" => lambda { "<#{SecureRandom.uuid}@#{ENV["HOST"]}>" }
   default from: ENV["SUPPORT_EMAIL"]
   layout "mailer"
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,10 +68,10 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: ENV["HOST"] }
   config.action_mailer.smtp_settings = {
-    address: "smtp.sendgrid.net",
+    address: "email-smtp.us-east-1.amazonaws.com",
     port: 587,
-    user_name: "apikey",
-    password: ENV["SENDGRID_API_KEY"],
+    user_name: ENV["MAIL_USERNAME"],
+    password: ENV["MAIL_PASSWORD"],
     authentication: "plain",
     enable_starttls_auto: true,
   }
