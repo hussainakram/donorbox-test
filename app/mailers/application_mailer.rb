8754fc5ac7420e33e7c2d67cfec3940f4d645f0d
hussainakram <hussain.akram128@gmail.com>
2021-06-09 14:20:59 +0500

    add env for support email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 class ApplicationMailer < ActionMailer::Base
-  default from: "support@donorbox-test.com"
+  default from: ENV["SUPPORT_EMAIL"]
   layout "mailer"
 end
