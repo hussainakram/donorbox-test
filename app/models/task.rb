b70eb4b54a20afab4ee8271fbe5e0f1d2b765cd4
hussainakram <hussain.akram128@gmail.com>
2021-06-09 17:45:21 +0500

    add search method in task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,13 +1,30 @@
 # frozen_string_literal: true
 class Task < ApplicationRecord
+  extend DateTimeHelpers
+
   belongs_to :user
 
   validates :title, presence: true, uniqueness: { scope: :user_id }
   validates :status, presence: true
 
-  enum status: [:open, :work_in_progress, :closed]
+  enum status: { open: 0, work_in_progress: 1, closed: 2 }
 
   after_create :add_tag_on_active_campaign, if: :first_task?
+
+  scope :by_user, ->(user) { where(user: user) if user.present? }
+
+  def self.search(search_by:, value:)
+    case search_by
+    when :title
+      where("title ilike :title", title: "%#{value}%")
+    when :status
+      where(status: statuses[value.to_sym])
+    when :created_at
+      where(created_at: date_range_of(value))
+    else
+      []
+    end
+  end
 
   def add_tag_on_active_campaign
     # TODO: replace perform_now with perform_later
