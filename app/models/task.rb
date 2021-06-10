87461db29e1668238297894d5f5964c1c0750cc5
hussainakram <hussain.akram128@gmail.com>
2021-06-10 14:38:53 +0500

    update task method

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 class Task < ApplicationRecord
   extend DateTimeHelpers
+  SEARCH_COLUMN_NAMES = { Title: "title", Status: "status", Created_At: "created_at" }
 
   belongs_to :user
 
@@ -14,15 +15,15 @@ class Task < ApplicationRecord
   scope :by_user, ->(user) { where(user: user) if user.present? }
 
   def self.search(search_by:, value:)
-    case search_by
+    case search_by.to_sym
     when :title
       where("title ilike :title", title: "%#{value}%")
     when :status
-      where(status: statuses[value.to_sym])
+      where(status: value.to_sym)
     when :created_at
       where(created_at: date_range_of(value))
     else
-      []
+      all
     end
   end
 
