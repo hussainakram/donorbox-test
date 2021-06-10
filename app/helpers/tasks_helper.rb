ddece1e9beac17eaeff4d25d41464f1a72b32013
Hussain Akram <hussain.akram128@gmail.com>
2021-06-11 04:27:37 +0500

    Add Design for main page (#17)

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,3 +1,13 @@
 # frozen_string_literal: true
 module TasksHelper
+  def status_class_name(status)
+    case status.to_sym
+    when :open
+      "bg-yellow-400"
+    when :work_in_progress
+      "bg-indigo-400"
+    when :closed
+      "bg-green-400"
+    end
+  end
 end
