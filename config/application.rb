1bcea7731ab0b27c9b66157e6c1a997d4ff1d60f
hussainakram <hussain.akram128@gmail.com>
2021-06-11 13:39:25 +0500

    revert time_zone change

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module DonorboxTest
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    config.time_zone = "Asia/Karachi"
+    # config.time_zone = "Central Time (US & Canada)"
     config.eager_load_paths << Rails.root.join("lib")
     config.eager_load_paths << Rails.root.join("services")
 
